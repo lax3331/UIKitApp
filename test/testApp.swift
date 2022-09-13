76d4f323e2d859320135285877e17a496e758877
Nikky <la2life1234@gmail.com>
2022-09-13 11:58:42 +0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Nikita Kuznetsov on 13.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
