c6f2c6964186a0ebecf19605af4d8d4312c441ef
Nikky <la2life1234@gmail.com>
2022-12-17 17:29:31 +0300

    circle lights + enum

--- Traffic Light/Traffic Light/ViewController.swift
+++ Traffic Light/Traffic Light/ViewController.swift
@@ -8,13 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var startButton: UIButton!
+    @IBOutlet var actionButton: UIButton!
+    
+    @IBOutlet var redLight: UIView!
+    
+    @IBOutlet var yellowLight: UIView!
+    
+    @IBOutlet var greenLight: UIView!
+    
+    enum lights {
+        case red
+        case yellow
+        case greeen
+        case off
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        redLight.layer.cornerRadius = 40
+        greenLight.layer.cornerRadius = 40
+        yellowLight.layer.cornerRadius = 40
+        actionButton.layer.cornerRadius = 20
+    
     }
-
+    @IBAction func changeColour() {
+    }
+    
 
 }
 
