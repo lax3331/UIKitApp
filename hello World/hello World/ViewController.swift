96e5977feb26f46792006ec07b80d7e9554e00cf
Nikky <la2life1234@gmail.com>
2022-12-17 13:56:47 +0300

    its work

--- hello World/hello World/ViewController.swift
+++ hello World/hello World/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var toggleButton: UIButton!
+    @IBOutlet var helloWorldLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        helloWorldLabel.isHidden = true
+        toggleButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func makeButtonAction() {
+        if helloWorldLabel.isHidden{
+            helloWorldLabel.isHidden = false
+            toggleButton.setTitle("Hide Text", for: .normal)
+        } else {
+            helloWorldLabel.isHidden = true
+            toggleButton.setTitle("Not Disturb", for: .normal)
+        }
+    }
+    
 }
 
